456386222a4f6d5d9096a74f3a7e341abe586ebb
liurui <liu.rui@oyohotels.cn>
2019-07-20 22:22:55 +0800

    Initial commit

---
+++ guns-api/src/main/java/com/stylefeng/guns/api/user/UserAPI.java
@@ -0,0 +1,11 @@
+package com.stylefeng.guns.api.user;
+
+/**
+ * @author liurui
+ * @time 2019-07-20 19:05
+ */
+public interface UserAPI {
+
+    boolean login(String username, String password);
+
+}
